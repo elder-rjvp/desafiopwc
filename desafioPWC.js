7e74b0212a48274d64255c440b6c9aca05607a0e
Elder_rjvp <elder.rjvp@gmail.com>
2023-07-11 17:18:30 -0300

    Add files via upload
    adicionado itens 3, 4 e 5 do desafio pwc

--- desafioPWC.js
+++ desafioPWC.js
@@ -21,3 +21,63 @@ fraseNova = aux.join(" ");
 
 console.log(fraseNova);
 
+//item 3
+
+frase = "babad";
+count = 0;
+aux = [];
+
+for(i = 0; i < frase.length; i++){
+    
+    if(frase[frase.length - (i + 1)] === frase[i]){
+    
+        aux[count] = frase[i];
+        
+        count++;
+    
+    }
+    
+}
+fraseNova = aux.join("")
+console.log(fraseNova);
+
+//item 4
+
+frase = "hello, how are you? i'm fine, thank you"
+
+aux = frase.split(" ");
+
+for (var i = 0; i < aux.length; i++) {
+    
+    if(i < aux.length -2){
+    
+        aux[i] = aux[i].charAt(0).toUpperCase() + aux[i].slice(1); 
+    
+    }
+    
+  }
+
+fraseNova = aux.join(" ");
+console.log(fraseNova);
+
+
+//item 5
+
+frase = "racecar";
+count = 0;
+aux = [];
+
+for(i = 0; i < frase.length; i++){
+    
+    aux[i] = frase[frase.length - (i + 1)];
+    
+}
+
+fraseNova = aux.join("")
+
+if(fraseNova === frase){
+    
+    console.log(true);
+    
+}
+
